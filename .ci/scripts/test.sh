67c818a67f3870e0e5591ef7a66a1b79abb5af0a
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-12-15 14:48:54 +0000

    Enable JUnit

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -7,4 +7,14 @@ go mod verify
 go-licenser -d
 go run .ci/scripts/check_format.go
 go run .ci/scripts/check_lint.go
-go test -v ./...
+
+# Run the tests
+set +e
+export OUT_FILE="build/test-report.out"
+mkdir -p build
+go test "./..." -v 2>&1 | tee ${OUT_FILE}
+status=$?
+go get -v -u github.com/jstemmer/go-junit-report
+go-junit-report > "build/junit-${GO_VERSION}.xml" < ${OUT_FILE}
+
+exit ${status}
