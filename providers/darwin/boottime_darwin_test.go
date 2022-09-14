8db68eb70fe5fde5f8efa38d95a124fd7e0eedcb
kruskall <99559985+kruskall@users.noreply.github.com>
2022-09-14 13:51:25 +0200

    feat: implement darwin boottime without using cgo (#131)
    * feat: implement darwin boottime without using cgo
    
    Use sysctl from the stdlib and add a test to make sure the
    implementation works with CGO_ENABLED=0
    
    * fix: rename test file to avoid compilation error on other systems
    
    * test: update method name
    
    Co-authored-by: Andrew Kroh <andrew.kroh@elastic.co>
    
    Co-authored-by: Andrew Kroh <andrew.kroh@elastic.co>

--- providers/darwin/boottime_darwin_test.go
+++ providers/darwin/boottime_darwin_test.go
@@ -15,26 +15,18 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//go:build amd64 || arm64
-// +build amd64 arm64
-
 package darwin
 
 import (
-	"fmt"
+	"testing"
 	"time"
 
-	"golang.org/x/sys/unix"
+	"github.com/stretchr/testify/assert"
 )
 
-const kernBoottimeMIB = "kern.boottime"
+func TestBootTime(t *testing.T) {
+	bt, err := BootTime()
+	assert.NoError(t, err)
 
-func BootTime() (time.Time, error) {
-	tv, err := unix.SysctlTimeval(kernBoottimeMIB)
-	if err != nil {
-		return time.Time{}, fmt.Errorf("failed to get host uptime: %w", err)
-	}
-
-	bootTime := time.Unix(int64(tv.Sec), int64(tv.Usec)*int64(time.Microsecond))
-	return bootTime, nil
+	t.Logf(bt.Format(time.RFC1123))
 }
