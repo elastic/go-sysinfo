00761d95a76b789def8cc11529d01aebbbafda28
Anderson Queiroz <anderson.queiroz@elastic.co>
2023-01-27 11:38:04 +0100

    enable integration tests

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -37,7 +37,7 @@ fi
 set +e
 export OUT_FILE="build/test-report.out"
 mkdir -p build
-go test "./..." -v 2>&1 | tee ${OUT_FILE}
+go test -tags integration "./..." -v 2>&1 | tee ${OUT_FILE}
 status=$?
 go install github.com/jstemmer/go-junit-report/v2@latest
 go-junit-report > "build/junit-${GO_VERSION}.xml" < ${OUT_FILE}
