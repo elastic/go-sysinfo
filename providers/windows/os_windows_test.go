28cbdc52b12c1c3ea994dc37e58f9cfcb6c1867a
Ben McNichols <mcnichols@gmail.com>
2023-05-18 15:19:10 -0400

    Fix major and minor version parsing on older Windows (#175)
    On Windows versions earlier than 10/2016, the `Major` version was
    getting overwritten with what would have been the `Minor` version, and
    the `Minor` version was not being set.
    
    Windows 7:
    Before fix:
    ```
    "os": {
      "type": "windows",
      "family": "windows",
      "platform": "windows",
      "name": "Windows 7 Enterprise",
      "version": "6.1",
      "major": 1,
      "minor": 0,
      "patch": 0,
      "build": "7601.0"
    },
    ```
    
    With fix:
    ```
    "os": {
      "type": "windows",
      "family": "windows",
      "platform": "windows",
      "name": "Windows 7 Enterprise",
      "version": "6.1",
      "major": 6,
      "minor": 1,
      "patch": 0,
      "build": "7601.0"
    },
    ```
    
    Windows 8:
    Before fix:
    ```
    "os": {
      "type": "windows",
      "family": "windows",
      "platform": "windows",
      "name": "Windows 8 Enterprise",
      "version": "6.2",
      "major": 2,
      "minor": 0,
      "patch": 0,
      "build": "9200.0"
    },
    ```
    
    With fix:
    ```
    "os": {
      "type": "windows",
      "family": "windows",
      "platform": "windows",
      "name": "Windows 8 Enterprise",
      "version": "6.2",
      "major": 6,
      "minor": 2,
      "patch": 0,
      "build": "9200.0"
    },
    ```
    
    Win 10:
    Separate code path, unimpacted, same before and after:
    ```
    "os": {
      "type": "windows",
      "family": "windows",
      "platform": "windows",
      "name": "Windows 11 Pro",
      "version": "10.0",
      "major": 10,
      "minor": 0,
      "patch": 0,
      "build": "22621.1702"
    },
    ```
    
    ---------
    
    Co-authored-by: Andrew Kroh <andrew.kroh@elastic.co>

--- providers/windows/os_windows_test.go
+++ providers/windows/os_windows_test.go
@@ -18,6 +18,9 @@
 package windows
 
 import (
+	"os/exec"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -90,4 +93,28 @@ func TestFixWindows11Naming(t *testing.T) {
 		fixWindows11Naming(tc.osInfo.Build, &tc.osInfo)
 		assert.Equal(t, tc.expectedName, tc.osInfo.Name)
 	}
+}
+
+func TestOperatingSystemMajorMinor(t *testing.T) {
+	// User PowerShell to get the expected OS version.
+	var major, minor int
+	if stdout, err := exec.Command("powershell.exe", "-c", "[System.Environment]::OSVersion.Version.Major").Output(); err != nil {
+		t.Fatal(err)
+	} else if major, err = strconv.Atoi(strings.TrimSpace(string(stdout))); err != nil {
+		t.Fatal(err)
+	}
+	if stdout, err := exec.Command("powershell.exe", "-c", "[System.Environment]::OSVersion.Version.Minor").Output(); err != nil {
+		t.Fatal(err)
+	} else if minor, err = strconv.Atoi(strings.TrimSpace(string(stdout))); err != nil {
+		t.Fatal(err)
+	}
+
+	// Verify expected output.
+	osInfo, err := OperatingSystem()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, major, osInfo.Major)
+	assert.Equal(t, minor, osInfo.Minor)
 }
