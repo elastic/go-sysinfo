be1652e2be02f60290bb857e34be2668c5b0e3ed
Andrew Kroh <andrew.kroh@elastic.co>
2023-05-18 14:57:57 -0400

    add TestOperatingSystemMajorMinor

--- providers/windows/os_windows_test.go
+++ providers/windows/os_windows_test.go
@@ -18,6 +18,8 @@
 package windows
 
 import (
+	"os/exec"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -90,4 +92,28 @@ func TestFixWindows11Naming(t *testing.T) {
 		fixWindows11Naming(tc.osInfo.Build, &tc.osInfo)
 		assert.Equal(t, tc.expectedName, tc.osInfo.Name)
 	}
+}
+
+func TestOperatingSystemMajorMinor(t *testing.T) {
+	// User PowerShell to get the expected OS version.
+	var major, minor int
+	if stdout, err := exec.Command("powershell.exe", "-c", "[System.Environment]::OSVersion.Version.Major").Output(); err != nil {
+		t.Fatal(err)
+	} else if major, err = strconv.Atoi(string(stdout)); err != nil {
+		t.Fatal(err)
+	}
+	if stdout, err := exec.Command("powershell.exe", "-c", "[System.Environment]::OSVersion.Version.Minor").Output(); err != nil {
+		t.Fatal(err)
+	} else if minor, err = strconv.Atoi(string(stdout)); err != nil {
+		t.Fatal(err)
+	}
+
+	// Verify expected output.
+	osInfo, err := OperatingSystem()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, major, osInfo.Major)
+	assert.Equal(t, minor, osInfo.Minor)
 }
