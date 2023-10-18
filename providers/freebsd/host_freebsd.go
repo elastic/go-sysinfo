eb0a11612fa513fd964f90dce7eb0a83418c35d0
Saro G. <sarog@users.noreply.github.com>
2023-10-18 13:33:19 -0400

    Added FreeBSD LoadAverage() support

--- providers/freebsd/host_freebsd.go
+++ providers/freebsd/host_freebsd.go
@@ -23,6 +23,7 @@ package freebsd
 import "C"
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"time"
@@ -82,6 +83,21 @@ func (h *host) Memory() (*types.HostMemoryInfo, error) {
 
 func (h *host) FQDN() (string, error) {
 	return shared.FQDN()
+}
+
+func (h *host) LoadAverage() (*types.LoadAverageInfo, error) {
+	load, err := getLoadAverage()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get loadavg: %w", err)
+	}
+
+	scale := float64(load.scale)
+
+	return &types.LoadAverageInfo{
+		One:     float64(load.load[0]) / scale,
+		Five:    float64(load.load[1]) / scale,
+		Fifteen: float64(load.load[2]) / scale,
+	}, nil
 }
 
 func newHost() (*host, error) {
