80edf3d8ca8fccc3113c085a617847e8d76c9c48
Leszek Kubik <39905449+intxgo@users.noreply.github.com>
2024-02-12 11:25:12 +0100

    adds HostInfo.NativeArchitecture (#200)

--- providers/windows/arch_windows.go
+++ providers/windows/arch_windows.go
@@ -18,14 +18,46 @@
 package windows
 
 import (
-	windows "github.com/elastic/go-windows"
+	"golang.org/x/sys/windows"
+
+	gowindows "github.com/elastic/go-windows"
+)
+
+const (
+	imageFileMachineAmd64 = 0x8664
+	imageFileMachineArm64 = 0xAA64
+	archIntel             = "x86_64"
+	archArm64             = "arm64"
 )
 
 func Architecture() (string, error) {
-	systemInfo, err := windows.GetNativeSystemInfo()
+	systemInfo, err := gowindows.GetNativeSystemInfo()
 	if err != nil {
 		return "", err
 	}
 
 	return systemInfo.ProcessorArchitecture.String(), nil
+}
+
+func NativeArchitecture() (string, error) {
+	var processMachine, nativeMachine uint16
+	// the pseudo handle doesn't need to be closed
+	var currentProcessHandle = windows.CurrentProcess()
+
+	err := windows.IsWow64Process2(currentProcessHandle, &processMachine, &nativeMachine)
+	if err != nil {
+		return "", err
+	}
+
+	var nativeArch string
+
+	switch nativeMachine {
+	case imageFileMachineAmd64:
+		// for parity with Architecture() as amd64 and x86_64 are used interchangeably
+		nativeArch = archIntel
+	case imageFileMachineArm64:
+		nativeArch = archArm64
+	}
+
+	return nativeArch, nil
 }
